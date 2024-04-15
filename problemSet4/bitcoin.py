984a55a52107098caf5fd5abb22fb1183efa57b3
imoleFolio <dremkay71@gmail.com>
2024-04-15 11:18:07 +0100

    problem set 4: bitcoin

---
+++ problemSet4/bitcoin.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+
+if len(sys.argv) == 1:
+    sys.exit("Missing command-line argument")
+
+try:
+    num = float(sys.argv[1])
+except Exception:
+    sys.exit("Command-line argument is not a number")
+
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+try:
+    result = requests.get(url).json()
+    price = result['bpi']['USD']['rate_float']
+    total = price * num
+    print(f"${total:,.4f}")
+except requests.RequestException as e:
+    print(e)
